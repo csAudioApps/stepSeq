186a88eb6e46fff72df45ec8b811bcbfb8060320
Thomas Lutz <tlutz65@gmail.com>
2020-07-21 15:05:10 -0700

    webpack config

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -1,9 +1,7 @@
-import * as React from 'react';
-
-// : React.FC
+import React from 'react';
 
 const App = (props) => {
-  return(<div>Piss off, I'm hungry</div>);
+  return(<div>Piss off, I'm full</div>);
 }
 
 export default App;
