4e9ead5e981c480604aa3b1e1db5ff6e72a8379e
Thomas Lutz <tlutz65@gmail.com>
2020-07-22 16:42:15 -0700

    add initial state and toggle grid button reducer case

--- src/client/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App.jsx";
-import './styles/styles.scss';
-
-const root = document.createElement("div");
-root.id = "root"
-document.body.appendChild(root);
-ReactDOM.render(<App />, root);
